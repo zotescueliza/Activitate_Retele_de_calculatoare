2a06de08038197c45e170391d2d26ae87fc566e2
Zotescu Eliza <zotescumaria23@stud.ase.ro>
2026-04-25 11:18:32 +0300

    Stergere fisiere Seminar 9 din root

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "Server UDP" cmd /k "title Server UDP & python server.py"
-timeout /t 1 /nobreak >nul
-start "Client 1" cmd /k "title Client 1 & python client.py"
-start "Client 2" cmd /k "title Client 2 & python client.py"
-start "Client 3" cmd /k "title Client 3 & python client.py"
